daec5d0cec236ab337a973e38bb3fdccc66109e3
williansbeato <willians_beato@hotmail.com>
2020-02-22 20:26:12 -0300

    Campos Obrigatorios

--- app/src/main/java/com/example/calculadora_gorjeta/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_gorjeta/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.calculadora_gorjeta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.net.Authenticator
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,39 +16,49 @@ class MainActivity : AppCompatActivity() {
     }
 
         fun cal(view : View){
+
+
             val valorConta : String = txtValorConta.text.toString()
             val gorjeta : String = txtGorjetaPorc.text.toString()
-
-
-            var resul : Double
-
-            resul = (gorjeta.toDouble() * valorConta.toDouble())
-
-            var resulGorjeta : Double
-
-            var divisor = 100
-
-            resulGorjeta = (resul / divisor)
-
-            var textGorjeta = "Gorjeta (R$): $resulGorjeta"
-
-            oResul.setText(textGorjeta)
-
-            var totalCont = (resulGorjeta + valorConta.toDouble())
-
-            var textTotalConta = "Total da Conta: $totalCont"
-
-            totalConta.setText(textTotalConta)
-
             val quantiPessoa : String = totalPessoas.text.toString()
 
-            var valorPorPessoa = (totalCont / quantiPessoa.toDouble())
 
-            var resulPessoa = " Total de $valorPorPessoa por pessoa"
+            if(TextUtils.isEmpty(valorConta) or TextUtils.isEmpty(gorjeta) or TextUtils.isEmpty(quantiPessoa) ){
 
-            valorPessoa.setText(resulPessoa)
+                txtValorConta.error = "Campo Obrigatorio"
+                txtGorjetaPorc.error = "Campo Obrigatorio"
+                totalPessoas.error = "Campo Obrigatorio"
+            }
+            else{
+
+                var resul : Double
+
+                resul = (gorjeta.toDouble() * valorConta.toDouble())
+
+                var resulGorjeta : Double
+
+                var divisor = 100
+
+                resulGorjeta = (resul / divisor)
+
+                var textGorjeta = "Gorjeta (R$): $resulGorjeta"
+
+                oResul.setText(textGorjeta)
+
+                var totalCont = (resulGorjeta + valorConta.toDouble())
+
+                var textTotalConta = "Total da Conta: $totalCont"
+
+                totalConta.setText(textTotalConta)
+
+                var valorPorPessoa : Double = (totalCont / quantiPessoa.toDouble())
+
+                var resulPessoa = " Total de R$ $valorPorPessoa por pessoa"
+
+                valorPessoa.setText(resulPessoa)
+
+            }
 
         }
-
 
 }
